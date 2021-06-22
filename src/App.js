2d0a273ee996148ed56c7a9bcb40d9a0f5b23a9f
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-06-22 22:16:01 +0200

    AddAssignment component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AddAssignment from "./components/AddAssignment";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AddAssignment />
     </div>
   );
 }
--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function AddAssignment() {
   return (
     <div>
       <form>
-        
+        <div className="form-control">
+          <label htmlFor="type">Assessment Type</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="module">module</label>
+          <input type="text" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="date">Due Date</label>
+          <input type="text" />
+        </div>
       </form>
     </div>
-  )
+  );
 }
