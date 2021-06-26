8ce8b45436c543810f7189ab1ec730608c05ce3d
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-06-26 20:04:25 +0200

    Delete functionality complete through binding and passing this, the id and dispatch on the event handler which points to a function that runs the dispatch and passes into it the action type and payload (Id) in this case

--- src/components/assessment.js
+++ src/components/assessment.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../styling/assessment.css";
+import { Consumer } from "../context";
 import deleteIcon from "../assets/deleteIcon.png";
 import completeIcon from "../assets/completeIcon.png";
 
 export default class Assessment extends Component {
-  deleteBtn = (e) => {
+  deleteBtn = (id, dispatch, e) => {
     e.preventDefault();
     console.log("delete me!");
+    dispatch({ type: "DELETE", payload: id });
   };
 
   completeBtn = (e) => {
@@ -17,24 +19,36 @@ export default class Assessment extends Component {
 
   render() {
     const { assessmentObj } = this.props;
+    const { id } = assessmentObj;
     return (
-      <div className="assessment">
-        <div className="assessment-info">
-          <h1>{assessmentObj.type}</h1>
-          <h3>{assessmentObj.module}</h3>
-          <p>{assessmentObj.description}</p>
-          <p id="date">{assessmentObj.dueDate}</p>
-        </div>
-        <div className="actionLinks">
-          <a href="#" onClick={this.completeBtn} className="completeIcon">
-            <img src={completeIcon} />
-          </a>
+      <Consumer>
+        {(value) => {
+          const { dispatch } = value;
+          return (
+            <div className="assessment">
+              <div className="assessment-info">
+                <h1>{assessmentObj.type}</h1>
+                <h3>{assessmentObj.module}</h3>
+                <p>{assessmentObj.description}</p>
+                <p id="date">{assessmentObj.dueDate}</p>
+              </div>
+              <div className="actionLinks">
+                <a href="#" onClick={this.completeBtn} className="completeIcon">
+                  <img src={completeIcon} />
+                </a>
 
-          <a href="#" className="deleteIcon" onClick={this.deleteBtn}>
-            <img src={deleteIcon} alt="deleteIcon" />
-          </a>
-        </div>
-      </div>
+                <a
+                  href="#"
+                  className="deleteIcon"
+                  onClick={this.deleteBtn.bind(this, id, dispatch)}
+                >
+                  <img src={deleteIcon} alt="deleteIcon" />
+                </a>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
