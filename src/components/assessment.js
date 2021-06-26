0bde85dc359c44df1c64f9292da241d09a5f9524
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-06-26 17:58:39 +0200

    added action links for deletion and completion marking

--- src/components/assessment.js
+++ src/components/assessment.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../styling/assessment.css";
+import deleteIcon from "../assets/deleteIcon.png";
+import completeIcon from "../assets/completeIcon.png";
 
 export default class Assessment extends Component {
   render() {
     const { assessmentObj } = this.props;
     return (
       <div className="assessment">
-        <h1>{assessmentObj.type}</h1>
-        <h3>{assessmentObj.module}</h3>
-        <p>{assessmentObj.description}</p>
-        <p id="date">{assessmentObj.dueDate}</p>
+        <div className="assessment-info">
+          <h1>{assessmentObj.type}</h1>
+          <h3>{assessmentObj.module}</h3>
+          <p>{assessmentObj.description}</p>
+          <p id="date">{assessmentObj.dueDate}</p>
+        </div>
+        <div className="actionLinks">
+          <a href="#" className="completeIcon">
+            <img src={completeIcon} />
+          </a>
+
+          <a href="#" className="deleteIcon">
+            <img src={deleteIcon} alt="deleteIcon" />
+          </a>
+        </div>
       </div>
     );
   }
