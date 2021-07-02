9066ea4fce63f22f0ba61728f444f077d1639e04
Koketso Mawasha <Koketsomawasha@gmail.com>
2021-07-02 12:26:58 +0200

    created new About component, pending route

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h1>About Self Portal</h1>
+        <p>
+          The self student portal was developed to facilitate the viewing,
+          adding and makring of student's individual academic progress and
+          position with their relevant workload.
+        </p>
+        <p>
+          The portal also strives to evolve into a real-time portal allowing
+          students to post key lecture points and notes or class activities
+          during lectures for later group, review, engage & spark learning
+          conversations even amongst learners that could not make it on campus
+          for various reasons. Keeping all learners fully engaged and up to
+          date.{" "}
+        </p>
+        <p>
+          In the near future, more features are anticipated to improve the
+          learning experience for hybrid online and contact learning
+          environments.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
